b942bd261ca72c0d57b7416594d7b63ec86ab131
Dmitriy Vinnik <dmitriy.v@2muchcoffee.com>
2018-11-15 19:19:16 +0200

    added exit on double back button

--- src/components/App/AppNavigator/index.tsx
+++ src/components/App/AppNavigator/index.tsx
@@ -5,6 +5,7 @@ import RootToaster from './RootToaster';
 import CustomersNavigator from './CustomersNavigator';
 import ProductsNavigator from './ProductsNavigator';
 import InvoicesNavigator from './InvoicesNavigator';
+import style from './style';
 
 const Navigator = createBottomTabNavigator({
   Invoices: InvoicesNavigator,
@@ -30,12 +31,93 @@ const Navigator = createBottomTabNavigator({
   },
 });
 
-export default class AppNavigator extends React.Component {
+// export default class AppNavigator extends React.Component {
+//   render() {
+//     return (
+//       <View style={{flex: 1}}>
+//         <Navigator/>
+//         <RootToaster/>
+//       </View>
+//     );
+//   }
+// }
+
+import { BackHandler, Dimensions, Animated, TouchableOpacity, Text } from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
+export default class AppNavigator extends React.Component<any, any> {
+
+  state = {
+    backClickCount: 0,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.springValue = new Animated.Value(100);
+
+  }
+
+  componentWillMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
+  }
+
+  _spring() {
+    this.setState({backClickCount: 1}, () => {
+      Animated.sequence([
+        Animated.spring(
+          this.springValue,
+          {
+            toValue: -.15 * height,
+            friction: 5,
+            duration: 300,
+            useNativeDriver: true,
+          },
+        ),
+        Animated.timing(
+          this.springValue,
+          {
+            toValue: 100,
+            duration: 300,
+            useNativeDriver: true,
+          },
+        ),
+
+      ]).start(() => {
+        this.setState({backClickCount: 0});
+      });
+    });
+
+  }
+
+  handleBackButton = () => {
+    this.state.backClickCount == 1 ? BackHandler.exitApp() : this._spring();
+
+    return true;
+  }
+
   render() {
+
     return (
-      <View style={{flex: 1}}>
+      <View style={style.container}>
         <Navigator/>
         <RootToaster/>
+
+        <Animated.View style={[style.animatedView, {transform: [{translateY: this.springValue}]}]}>
+          <Text style={style.exitTitleText}>Press back again to exit the app</Text>
+          <TouchableOpacity
+            activeOpacity={0.9}
+            onPress={() => BackHandler.exitApp()}
+            style={style.buttonExit}
+          >
+            <Text style={style.exitText}>Exit</Text>
+          </TouchableOpacity>
+        </Animated.View>
       </View>
     );
   }
